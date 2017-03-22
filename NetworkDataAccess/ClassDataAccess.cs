1dd80f82e6d05ca5c9d4dd259c3f82dcd7abf1f7
Heshitha Chathuranga <heshithachathuranga@gmail.com>
2017-03-22 14:06:09 +0530

    Changes

---
+++ NetworkDataAccess/ClassDataAccess.cs
@@ -0,0 +1,71 @@
+﻿using NetworkModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkDataAccess
+{
+    public class ClassDataAccess
+    {
+        static NetworkMarketingDataContext db = new NetworkMarketingDataContext();
+        public static List<ViewClassUser> GetClassDetailsForViewClass(int ClassID)
+        {
+            List<ViewClassUser> retVal = new List<ViewClassUser>();
+            try
+            {
+                var ClassDetails = db.Classes.Where(x => x.ClassID == ClassID).FirstOrDefault();
+                if (ClassDetails != null)
+                {
+                    foreach (var item in ClassDetails.ClassUsers)
+                    {
+                        ViewClassUser vcu = new ViewClassUser()
+                        {
+                            UserID = item.User.UserID,
+                            FirstName = item.User.FirstName,
+                            LastName = item.User.LastName,
+                            UserName = item.User.Username,
+                            ImageExt = item.User.ImageExt,
+                            FollowerCount = item.User.LeaderFollowers.Where(x => x.LeaderClassID == ClassDetails.ClassID).Count(),
+                            Followers = item.User.LeaderFollowers.Where(x => x.LeaderClassID == ClassDetails.ClassID).Select(x => new ViewClassFollower() { 
+                                Name = x.Follower.FirstName + " " + x.Follower.LastName,
+                                UserID = x.Follower.UserID,
+                                ImageExt = x.Follower.ImageExt
+                            }).ToList()
+                        };
+
+                        retVal.Add(vcu);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return retVal;
+        }
+
+        public static int[] GetClassHistory(int UserID)
+        {
+            int[] retVal = new int[10];
+            try
+            {
+                var result = db.Users.Where(x => x.UserID == UserID).FirstOrDefault();
+                if (result != null)
+                {
+                    var resultList = result.ClassUsers.Select(x => x.ClassID.Value).Distinct().OrderByDescending(x => x);
+                    retVal = resultList.ToArray();
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return retVal;
+        }
+    }
+
+
+}
---
+++ NetworkModels/ViewClassModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkModels
+{
+    public class ViewClassModel
+    {
+
+    }
+
+    public class ViewClassUser
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public int FollowerCount { get; set; }
+        public List<ViewClassFollower> Followers { get; set; }
+
+        public string LastName { get; set; }
+
+        public string ImageExt { get; set; }
+    }
+
+    public class ViewClassFollower
+    {
+        public int UserID { get; set; }
+        public string Name { get; set; }
+
+        public string ImageExt { get; set; }
+    }
+}
